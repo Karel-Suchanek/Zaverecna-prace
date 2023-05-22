565fa5095b5ea9f37791dcc41ac0b3a227908858
Karel Suchánek <suchanek.karel.95@gmail.com>
2023-05-22 17:51:42 +0200

    finalcomit

---
+++ Závěrečná práce záloha 1513/obsluha.js
@@ -0,0 +1,2 @@
+let pojistenec = new Pojistenec();
+pojistenec.vypisZaznamy();
---
+++ nový pokus záloha/index.js
@@ -0,0 +1,95 @@
+class Zaznam {
+    constructor(jmeno, prijmeni, vek, telefon){
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni,
+        this.vek = vek;
+        this.telefon = telefon;
+
+    }
+}
+
+class Pojistenec {
+
+    constructor(jazyk = "cs-CZ") {
+        const zaznamyZeStorage = localStorage.getItem("zaznamy");
+        this.zaznamy = zaznamyZeStorage ? JSON.parse(zaznamyZeStorage) : [];
+        this.jazyk = jazyk;
+
+        this.jmeno = document.getElementById("jmeno");
+        this.prijmeni = document.getElementById("prijmeni");
+        this.vek = document.getElementById("vek");
+        this.telefon = document.getElementById("telefon");
+        this.uloz = document.getElementById("uloz")
+        this.vypisElement = document.getElementById("seznam-pojistencu");
+
+        this.nastavUdalosti();
+    }
+    nastavUdalosti(){
+        this.uloz.addEventListener("click", () => {
+
+
+            const zaznam = new Zaznam(this.jmeno.value, this.prijmeni.value, this.vek.value, this.telefon.value);
+            console.log(zaznam);
+            this.zaznamy.push(zaznam);
+
+         
+           this.kontrolaCislisVeJmeneAPrijmeni();
+           this.vypisZaznamy();
+
+        });
+    }
+
+    ulozZaznamy() {
+        localStorage.setItem("zaznamy", JSON.stringify(this.zaznamy));
+    
+    }
+
+    vypisZaznamy() {
+        this.vypisElement.innerHTML = "";
+        for (let zaznam of this.zaznamy) {
+            this.vypisElement.insertAdjacentHTML("beforeend",` <tr><td> ${zaznam.jmeno} </td> <td>  ${zaznam.prijmeni} </td> <td> ${zaznam.vek}</td> <td> ${zaznam.telefon} </td></tr>`);
+        }
+    }
+    kontrolaCislisVeJmeneAPrijmeni(){
+   
+        if(this.jmeno.value.includes(0) || this.prijmeni.value.includes(0))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+
+        else if(this.jmeno.value.includes(1) || this.prijmeni.value.includes(1))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+
+        else if(this.jmeno.value.includes(2) || this.prijmeni.value.includes(2))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+
+        else if(this.jmeno.value.includes(3) || this.prijmeni.value.includes(3))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+
+        else if(this.jmeno.value.includes(4) || this.prijmeni.value.includes(4))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+
+        else if(this.jmeno.value.includes(5) || this.prijmeni.value.includes(5))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+
+        else if(this.jmeno.value.includes(6) || this.prijmeni.value.includes(6))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+
+        else if(this.jmeno.value.includes(7) || this.prijmeni.value.includes(7))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+
+        else if(this.jmeno.value.includes(8) || this.prijmeni.value.includes(8))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+
+        else if(this.jmeno.value.includes(9) || this.prijmeni.value.includes(9))
+            alert("Jméno ani příjmení nemůže obsahovat číslo.");
+        else
+        this.ulozZaznamy();
+    }
+
+    
+}
+
+
+
+
+let pojistenec = new Pojistenec();
+pojistenec.vypisZaznamy();
